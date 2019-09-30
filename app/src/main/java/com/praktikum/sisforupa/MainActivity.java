20260af750a9c1a46446d2cb2009904299d22286
JohanAce <jonathangraciano113@gmail.com>
2019-09-30 11:28:50 +0700

    8
    Signed-off-by: JohanAce <jonathangraciano113@gmail.com>

--- app/src/main/java/com/praktikum/sisforupa/MainActivity.java
+++ app/src/main/java/com/praktikum/sisforupa/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnTelpon = findViewById(R.id.btnTelpon);
+        Button btnMaps = findViewById(R.id.btnMaps);
+
+        btnTelpon.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                telponNomor(v);
+            }
+
+            public void telponNomor(View v) {
+            }
+        });
+
+        btnMaps.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                bukaMaps(v);
+            }
+
+            public void bukaMaps(View v) {
+            }
+        });
     }
+
 
     public void simulasi(View view) {
         Intent intent = new Intent(this, simulasi.class);
@@ -32,11 +54,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void konsultasi(View view) {
-        Intent intent = new Intent(this, konsultasi.class);
+        Intent intent = new Intent(this, blueprint.class);
         startActivity(intent);
     }
     public void blueprint(View view) {
-        Intent intent = new Intent(this, blueprint.class);
+        Intent intent = new Intent(this, konsultasi.class);
         startActivity(intent);
     }
     public void saran(View view) {
--- app/src/main/java/com/praktikum/sisforupa/nilai.java
+++ app/src/main/java/com/praktikum/sisforupa/nilai.java
@@ -1,4 +1,14 @@
 package com.praktikum.sisforupa;
 
-class nilai {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nilai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nilai);
+    }
 }
